37eecdb92db2b7fb6b43fefdec111e587bcdc3ac
akunkel81 <ankunkel13@gmail.com>
2026-02-18 15:10:12 -0500

    Add files via upload

---
+++ Unit.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Unit : MonoBehaviour
+{
+    public string unitName;
+    public int unitLevel;
+    public int damage;
+    public int maxHP;
+    public int currentHP;
+
+}
